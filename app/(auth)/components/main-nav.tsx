00813cb9ab17b64a0bca4e674146d7c607b94013
chadmuro <chadmuro@gmail.com>
2024-03-17 22:41:02 +0900

    Update Icons component and add logo and menu icons

--- app/(auth)/components/main-nav.tsx
+++ app/(auth)/components/main-nav.tsx
@@ -6,7 +6,7 @@ import {
   DropdownMenu,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
 
 export function MainNav({
   className,
@@ -37,7 +37,7 @@ export function MainNav({
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="outline" size="icon" className="md:hidden">
-            <MenuIcon className="h-[1.2rem] w-[1.2rem]" />
+            <Icons.menu className="h-6 w-6" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
@@ -53,26 +53,5 @@ export function MainNav({
         </DropdownMenuContent>
       </DropdownMenu>
     </>
-  );
-}
-
-function MenuIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
   );
 }
--- app/(public)/components/header.tsx
+++ app/(public)/components/header.tsx
@@ -8,12 +8,13 @@ import {
   DropdownMenu,
 } from "@/components/ui/dropdown-menu";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Icons } from "@/components/icons";
 
 export default function Header() {
   return (
     <header className="flex items-center h-16 px-4 border-b w-full">
       <Link className="mr-auto" href="/">
-        <FlagIcon className="h-6 w-6" />
+        <Icons.logo className="h-6 w-6" />
         <span className="sr-only">Dev Reads</span>
       </Link>
       <nav className="hidden md:flex space-x-4">
@@ -39,7 +40,7 @@ export default function Header() {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="outline" size="icon" className="md:hidden">
-            <MenuIcon className="h-[1.2rem] w-[1.2rem]" />
+            <Icons.menu className="h-6 w-6" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
@@ -58,46 +59,5 @@ export default function Header() {
         <ModeToggle />
       </div>
     </header>
-  );
-}
-
-function FlagIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z" />
-      <line x1="4" x2="4" y1="22" y2="15" />
-    </svg>
-  );
-}
-
-function MenuIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
   );
 }
